70545d1b6581219bfdec53a612ba7c42462a0073
japanese-goblinn <cool45akol@gmail.com>
2021-04-13 00:39:51 +0300

    feat: migrated to swift ui

---
+++ PerfectMatch/PerfectMatch/PerfectMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectMatchApp.swift
+//  PerfectMatch
+//
+//  Created by Kirill on 13.04.21.
+//
+
+import SwiftUI
+
+@main
+struct PerfectMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
