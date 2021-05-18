62baf7deb9cc8ce0c14a1b9493650f9ced390a46
japanese-goblinn <cool45akol@gmail.com>
2021-05-18 19:53:42 +0300

    feat(Resources): color resources handling

--- PerfectMatchResources/Sources/PerfectMatchResources/Colors.swift
+++ PerfectMatchResources/Sources/PerfectMatchResources/Colors.swift
@@ -1,12 +1,28 @@
 import AppKit
+import SwiftUI
 import Foundation
 
-//public struct ColorResource {
-//  static var
-//}
-//
-//extension NSColor {
-//  static func resource(_ color: ColorResource) {
-//    self.ini
-//  }
-//}
+public extension Color {
+  static func asset(_ color: ColorResource) -> Self {
+    return .init(color.name, bundle: .module)
+  }
+}
+
+public extension NSColor {
+  static func asset(_ color: ColorResource) -> NSColor {
+    return .init(named: color.name, bundle: .module)!
+  }
+}
+
+public struct ColorResource {
+  public let name: String
+
+  internal init(name: String) {
+    self.name = name
+  }
+}
+
+public extension ColorResource {
+  static var primaryGreen: Self { .init(name: "PrimaryGreen") }
+  static var primaryRed: Self { .init(name: "PrimaryRed") }
+}
