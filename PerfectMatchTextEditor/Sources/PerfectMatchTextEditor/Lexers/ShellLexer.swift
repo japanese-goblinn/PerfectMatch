ec8faf3a601c3719f11e0625c933e41617ce98e1
japanese-goblinn <cool45akol@gmail.com>
2021-05-27 14:43:08 +0300

    feat(TextEditor): shell lexer update

--- PerfectMatchTextEditor/Sources/PerfectMatchTextEditor/Lexers/ShellLexer.swift
+++ PerfectMatchTextEditor/Sources/PerfectMatchTextEditor/Lexers/ShellLexer.swift
@@ -5,7 +5,7 @@ public class ShellLexer: SourceCodeRegexLexer {
   public lazy var generators: [TokenGenerator] = {
     var generators = [TokenGenerator?]()
   
-    let keywords = "exit set local if then else elif fi case esac for select while until do done in function time coproc".components(separatedBy: .whitespaces)
+    let keywords = "exit set local if then else elif fi case esac for select while until do done in function time coproc return".components(separatedBy: .whitespaces)
     generators.append(keywordGenerator(keywords, tokenType: .keyword))
     generators.append(regexGenerator("\\b(echo|printf)", tokenType: .identifier))
     // Digits
