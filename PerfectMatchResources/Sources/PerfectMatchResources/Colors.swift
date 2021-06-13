0f88a5b57d4c57da39a908ac299e2546daf55c0f
japanese-goblinn <cool45akol@gmail.com>
2021-06-13 23:44:56 +0300

    feat(Resources): colors update

--- PerfectMatchResources/Sources/PerfectMatchResources/Colors.swift
+++ PerfectMatchResources/Sources/PerfectMatchResources/Colors.swift
@@ -3,32 +3,37 @@ import SwiftUI
 import Foundation
 
 public extension ColorResource {
-  @inlinable static var primaryGreen: Self { .init(name: "PrimaryGreen") }
-  @inlinable static var primaryRed: Self { .init(name: "PrimaryRed") }
-  @inlinable static var darkThemeBackground: Self { .init(name: "DarkThemeBackground") }
+  static var primaryGreen: Self { .init(name: "PrimaryGreen", bundle: .module) }
+  static var primaryRed: Self { .init(name: "PrimaryRed", bundle: .module) }
+  static var darkThemeBackground: Self { .init(name: "DarkThemeBackground", bundle: .module) }
 }
 
 public extension Color {
+  @inlinable
   static func asset(_ color: ColorResource) -> Self {
-    return .init(color.name, bundle: .module)
+    return .init(color.name, bundle: color.bundle)
   }
 }
 
 public extension NSColor {
+  @inlinable
   static func asset(_ color: ColorResource) -> NSColor {
-    return .init(named: color.name, bundle: .module)!
+    return .init(named: color.name, bundle: color.bundle)!
   }
   
+  @inlinable
   static func asset(_ color: ColorResource, alpha: CGFloat) -> NSColor {
-    return .init(named: color.name, bundle: .module)!.withAlphaComponent(alpha)
+    return .init(named: color.name, bundle: color.bundle)!
+      .withAlphaComponent(alpha)
   }
 }
 
 public struct ColorResource {
   public let name: String
+  public let bundle: Bundle
 
-  @usableFromInline
-  internal init(name: String) {
+  internal init(name: String, bundle: Bundle) {
     self.name = name
+    self.bundle = bundle
   }
 }
